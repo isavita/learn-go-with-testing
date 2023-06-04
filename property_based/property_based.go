5584ded85e1a7ca776b7257c00f55cb36c12ca02
Aleksandar Dimov <isavitaisa@gmail.com>
2023-06-04 08:18:13 +0100

    add teh book implementation of ConvertToArabic

--- property_based/property_based.go
+++ property_based/property_based.go
@@ -19,7 +19,7 @@ func ArabToRoman(n int) string {
 			roman.WriteString(strings.Repeat(s, count))
 		}
 	}
-	roman.WriteString(baseRoman(n % 10))
+	roman.WriteString(baseRoman(n))
 	return roman.String()
 }
 
@@ -68,6 +68,135 @@ func baseRoman(n int) string {
 	return ""
 }
 
+func RomanToArabic(s string) int {
+	var n int
+	for i := 0; i < len(s); i++ {
+		currentSymbol := string(s[i])
+		currentValue := romanNumeralValue(currentSymbol)
+		if i < len(s)-1 {
+			nextSymbol := string(s[i+1])
+			nextValue := romanNumeralValue(nextSymbol)
+			if nextValue > currentValue {
+				currentValue = -currentValue
+			}
+		}
+		n += currentValue
+	}
+	return n
+}
+
+func romanNumeralValue(symbol string) int {
+	switch symbol {
+	case "I":
+		return 1
+	case "V":
+		return 5
+	case "X":
+		return 10
+	case "L":
+		return 50
+	case "C":
+		return 100
+	case "D":
+		return 500
+	case "M":
+		return 1000
+	default:
+		return 0
+	}
+}
+
 func findParts(n, part int) int {
 	return n / part
+}
+
+func RomanToArab(s string) int {
+	var n int
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(s, numeral.Symbol) {
+			n += numeral.Value
+			s = s[len(numeral.Symbol):]
+		}
+	}
+	return n
+}
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+type RomanNumerals []RomanNumeral
+
+func (numerals RomanNumerals) ValueOf(symbols ...byte) int {
+	roman := string(symbols)
+	for _, numeral := range numerals {
+		if numeral.Symbol == roman {
+			return numeral.Value
+		}
+	}
+	return 0
+}
+
+var allRomanNumerals = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(n int) string {
+	var roman strings.Builder
+	for _, numeral := range allRomanNumerals {
+		for n >= numeral.Value {
+			roman.WriteString(numeral.Symbol)
+			n -= numeral.Value
+		}
+	}
+	return roman.String()
+}
+
+func ConvertToArabic(roman string) (n int) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		n += allRomanNumerals.ValueOf(symbols...)
+	}
+	return
+}
+
+func (r RomanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, numeral := range r {
+		if numeral.Symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
+type windowedRoman string
+
+func (w windowedRoman) Symbols() (symbols [][]byte) {
+	for i := 0; i < len(w); i++ {
+		symbol := w[i]
+		if i+1 < len(w) && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]) {
+			symbols = append(symbols, []byte{symbol, w[i+1]})
+			i++
+		} else {
+			symbols = append(symbols, []byte{symbol})
+		}
+	}
+	return
+}
+
+func isSubtractive(symbol uint8) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
 }
--- property_based/property_based_test.go
+++ property_based/property_based_test.go
@@ -1,10 +1,12 @@
 package propertybased
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestNumConv(t *testing.T) {
+// My personal implementation
+func TestArabToRoman(t *testing.T) {
 	cases := []struct {
 		Description string
 		Number      int
@@ -36,6 +38,97 @@ func TestNumConv(t *testing.T) {
 	}
 }
 
+func TestRomanToArab(t *testing.T) {
+	cases := []struct {
+		Description string
+		Number      string
+		Want        int
+	}{
+		{"converts 0 to ''", "", 0},
+		{"converts 1 to 'I'", "I", 1},
+		{"converts 3 to 'III'", "III", 3},
+		{"converts 8 to 'VIII'", "VIII", 8},
+		{"converts 10 to 'X'", "X", 10},
+		{"convert 39 to 'XXXIX'", "XXXIX", 39},
+		{"convert 246 to 'CCXLVI'", "CCXLVI", 246},
+		{"convert 1776 to 'MDCCLXXVI'", "MDCCLXXVI", 1776},
+		{"convert 1918 to 'MCMXVIII'", "MCMXVIII", 1918},
+		{"convert 2023 to 'MMXXIII'", "MMXXIII", 2023},
+		{"convert 2421 to 'MMCDXXI'", "MMCDXXI", 2421},
+		{"converts 4 to 'IV'", "IV", 4},
+		{"converts 9 to 'IX'", "IX", 9},
+		{"converts 40 to 'XL'", "XL", 40},
+		{"converts 90 to 'XC'", "XC", 90},
+		{"converts 400 to 'CD'", "CD", 400},
+		{"converts 900 to 'CM'", "CM", 900},
+	}
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := RomanToArabic(test.Number)
+			if got != test.Want {
+				t.Errorf("got %d want %d", got, test.Want)
+			}
+		})
+	}
+}
+
+var cases = []struct {
+	Arabic int
+	Roman  string
+}{
+	{Arabic: 1, Roman: "I"},
+	{Arabic: 2, Roman: "II"},
+	{Arabic: 3, Roman: "III"},
+	{Arabic: 4, Roman: "IV"},
+	{Arabic: 5, Roman: "V"},
+	{Arabic: 6, Roman: "VI"},
+	{Arabic: 7, Roman: "VII"},
+	{Arabic: 8, Roman: "VIII"},
+	{Arabic: 9, Roman: "IX"},
+	{Arabic: 10, Roman: "X"},
+	{Arabic: 14, Roman: "XIV"},
+	{Arabic: 18, Roman: "XVIII"},
+	{Arabic: 20, Roman: "XX"},
+	{Arabic: 39, Roman: "XXXIX"},
+	{Arabic: 40, Roman: "XL"},
+	{Arabic: 47, Roman: "XLVII"},
+	{Arabic: 49, Roman: "XLIX"},
+	{Arabic: 50, Roman: "L"},
+	{Arabic: 100, Roman: "C"},
+	{Arabic: 90, Roman: "XC"},
+	{Arabic: 400, Roman: "CD"},
+	{Arabic: 500, Roman: "D"},
+	{Arabic: 900, Roman: "CM"},
+	{Arabic: 1000, Roman: "M"},
+	{Arabic: 1984, Roman: "MCMLXXXIV"},
+	{Arabic: 3999, Roman: "MMMCMXCIX"},
+	{Arabic: 2014, Roman: "MMXIV"},
+	{Arabic: 1006, Roman: "MVI"},
+	{Arabic: 798, Roman: "DCCXCVIII"},
+}
+
+func TestConvertToRoman(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
+			}
+		})
+	}
+}
+
+func TestConvertToArabic(t *testing.T) {
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
+			}
+		})
+	}
+}
+
 func assertString(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
@@ -46,6 +139,24 @@ func assertString(t testing.TB, got, want string) {
 
 func BenchmarkArabToRoman(t *testing.B) {
 	for i := 0; i < t.N; i++ {
-		_ = ArabToRoman(1776)
+		ArabToRoman(1776)
+	}
+}
+
+func BenchmarkRomanToArab(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		RomanToArab("MDCCLXXVI")
+	}
+}
+
+func BenchmarkConvertToRoman(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		ConvertToRoman(1776)
+	}
+}
+
+func BenchmarkConvertToArabic(t *testing.B) {
+	for i := 0; i < t.N; i++ {
+		ConvertToArabic("MDCCLXXVI")
 	}
 }
